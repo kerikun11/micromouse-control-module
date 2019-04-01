473549f03066aaabe4ba3fc781cf259e465fd126
Ryotaro Onuki <kerikun11+github@gmail.com>
2019-04-02 00:19:56 +0900

    namespace slalomを作成

---
+++ inc/slalom.h
@@ -0,0 +1,164 @@
+/**
+ * @file SlalomDesigner.h
+ * @author Ryotaro Onuki (kerikun11+github@gmail.com)
+ * @brief 拘束条件からスラロームを軌道生成するライブラリ
+ * @version 0.1
+ * @date 2019-04-01
+ *
+ * @copyright Copyright (c) 2019
+ *
+ */
+#pragma once
+
+#include "AccelDesigner.h"
+#include "Position.h"
+
+#include <array>
+#include <iomanip>
+
+namespace ctrl {
+
+namespace slalom {
+
+/* 走行定数 */
+static constexpr float m_dddth = 9600 * M_PI;
+static constexpr float m_ddth = 90 * M_PI;
+static constexpr float m_dth = 4 * M_PI;
+
+struct State {
+  float t = 0;
+  Position q;
+  Position dq;
+  Position ddq;
+  Position dddq;
+};
+
+struct Shape {
+  Position total;
+  Position curve;
+  float straight_prev;
+  float straight_post;
+  float v_ref;
+  Shape(const Position total, const Position curve, float straight_prev,
+        const float straight_post, const float v_ref)
+      : total(total), curve(curve), straight_prev(straight_prev),
+        straight_post(straight_post), v_ref(v_ref) {}
+  Shape(const Position total, const float y_curve_end, const float x_adv = 0)
+      : total(total) {
+    const float Ts = 1e-5; /**< サンプリング周期 */
+    float v = 600.0f;      /**< 初期値 */
+    struct State s;        /**< シミュレーションの状態 */
+    AccelDesigner ad;
+    for (int i = 0; i < 2; ++i) {
+      ad.reset(m_dddth, m_ddth, 0, m_dth, 0, total.th);
+      s.t = s.q.x = s.q.y = 0;
+      /* シミュレーション */
+      while (s.t < ad.t_end()) {
+        integrate(ad, &s, v, Ts);
+      }
+      integrate(ad, &s, v, s.t - ad.t_end());
+      v *= y_curve_end / s.q.y;
+    }
+    curve = s.q;
+    v_ref = v;
+    const float sin_th = std::sin(total.th);
+    const float cos_th = std::cos(total.th);
+    /* 前後の直線の長さを決定 */
+    if (std::abs(sin_th) < 0.001f) {
+      /* 180度ターン */
+      straight_prev = x_adv;
+      straight_post = x_adv;
+      curve = total;
+    } else {
+      /* 180度ターン以外 */
+      straight_prev = total.x - s.q.x - cos_th / sin_th * (total.y - s.q.y);
+      straight_post = 1 / sin_th * (total.y - s.q.y);
+    }
+  }
+  static void integrate(const AccelDesigner &ad, struct State *s, const float v,
+                        const float Ts) {
+    /* Preparation */
+    float &t = s->t;
+    /* Calculation */
+    const std::array<float, 3> th{ad.x(t), ad.x(t + Ts / 2), ad.x(t + Ts)};
+    std::array<float, 3> cos_th;
+    std::array<float, 3> sin_th;
+    for (int i = 0; i < 3; ++i) {
+      cos_th[i] = std::cos(th[i]);
+      sin_th[i] = std::sin(th[i]);
+    }
+    /* Integral */
+    s->q.x += v * Ts * (cos_th[0] + 4 * cos_th[1] + cos_th[2]) / 6;
+    s->q.y += v * Ts * (sin_th[0] + 4 * sin_th[1] + sin_th[2]) / 6;
+    s->t += Ts;
+    /* Result */
+    s->dq.x = v * cos_th[2];
+    s->dq.y = v * sin_th[2];
+    s->q.th = ad.x(t);
+    s->dq.th = ad.v(t);
+    s->ddq.th = ad.a(t);
+    s->dddq.th = ad.j(t);
+    s->ddq.x = -s->dq.y * s->dq.th;
+    s->ddq.y = +s->dq.x * s->dq.th;
+    s->dddq.x = -s->ddq.y * s->dq.th - s->dq.y * s->ddq.th;
+    s->dddq.y = +s->ddq.x * s->dq.th + s->dq.x * s->ddq.th;
+  }
+  /**
+   * @brief 情報の表示
+   */
+  std::ostream &printDefinition(std::ostream &os, std::string name) const {
+    os << "static const auto " << name << " = ";
+    os << "ctrl::slalom::Shape(";
+    os << "ctrl::Position(" << total.x << ", " << total.y << ", " << total.th
+       << "), ";
+    os << "ctrl::Position(" << curve.x << ", " << curve.y << ", " << curve.th
+       << "), ";
+    os << straight_prev << ", " << straight_post << ", " << v_ref;
+    os << ");";
+    os << std::endl;
+    return os;
+  }
+  /**
+   * @brief 情報の表示
+   */
+  friend std::ostream &operator<<(std::ostream &os, const Shape &obj) {
+    os << "SlalomDesigner" << std::endl;
+    os << "\ttotal: " << obj.total << std::endl;
+    os << "\tnet: " << obj.curve << std::endl;
+    os << "\tv_ref: " << obj.v_ref << std::endl;
+    os << "\tstraight_prev: " << obj.straight_prev << std::endl;
+    os << "\tstraight_post: " << obj.straight_post << std::endl;
+    auto end = Position(obj.straight_prev) + obj.curve +
+               Position(obj.straight_post).rotate(obj.curve.th);
+    os << "\terror: " << obj.total - end << std::endl;
+    return os;
+  }
+};
+
+class Trajectory {
+public:
+  Trajectory(const Shape shape) : shape(shape) {}
+  void reset(const float velocity) {
+    this->velocity = velocity;
+    const float gain = velocity / shape.v_ref;
+    ad.reset(gain * gain * gain * m_dddth, gain * gain * m_ddth, 0,
+             gain * m_dth, 0, shape.total.th);
+  }
+  void update(struct State *s, const float Ts) const {
+    return Shape::integrate(ad, s, velocity, Ts);
+  }
+  float t_end() const { return ad.t_end(); }
+  float get_v_ref() const { return shape.v_ref; }
+  const Position &get_net_curve() const { return shape.curve; }
+  float get_straight_prev() const { return shape.straight_prev; }
+  float get_straight_post() const { return shape.straight_post; }
+  const struct Shape &getShape() const { return shape; }
+
+private:
+  Shape shape;
+  AccelDesigner ad;
+  float velocity;
+};
+
+}; // namespace slalom
+} // namespace ctrl
--- test_traj/main.cpp
+++ test_traj/main.cpp
@@ -6,57 +6,71 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <string>
 
 std::ofstream of("out.csv");
 std::ostream &os = of;
 
 using namespace ctrl;
+// using namespace slalom;
 
-static auto sd_SL90 =
-    SlalomDesigner(SlalomDesigner::Shape(Position(45, 45, M_PI / 2), 40));
+// static auto SS_SL90 = slalom::Shape(Position(45, 45, M_PI / 2), 40);
+// static auto SS_FL45 = slalom::Shape(Position(90, 45, M_PI / 4), 25);
+// static auto SS_FL90 = slalom::Shape(Position(90, 90, M_PI / 2), 60);
+// static auto SS_FL135 = slalom::Shape(Position(45, 90, M_PI * 3 / 4), 80);
+// static auto SS_FL180 = slalom::Shape(Position(0, 90, M_PI), 90, 30);
+// static auto SS_FLV90 =
+//     slalom::Shape(Position(45 * std::sqrt(2), 45 * std::sqrt(2), M_PI / 2),
+//     42);
 
-static auto sd_FL45 =
-    SlalomDesigner(SlalomDesigner::Shape(Position(90, 45, M_PI / 4), 30));
+static const auto SS_SL90 = ctrl::slalom::Shape(
+    ctrl::Position(45, 45, 1.5708), ctrl::Position(39.9966, 39.9986, 1.5708),
+    5.00339, 5.00138, 372.034);
+static const auto SS_FL45 = ctrl::slalom::Shape(
+    ctrl::Position(90, 45, 0.785398),
+    ctrl::Position(60.3412, 25.0001, 0.785398), 9.65883, 28.2842, 586.486);
+static const auto SS_FL90 = ctrl::slalom::Shape(
+    ctrl::Position(90, 90, 1.5708), ctrl::Position(59.995, 60.0004, 1.5708),
+    30.005, 29.9996, 558.028);
+static const auto SS_FL135 = ctrl::slalom::Shape(
+    ctrl::Position(45, 90, 2.35619), ctrl::Position(33.1317, 79.9992, 2.35619),
+    21.8691, 14.1433, 510.551);
+static const auto SS_FL180 =
+    ctrl::slalom::Shape(ctrl::Position(0, 90, 3.14159),
+                        ctrl::Position(0, 90, 3.14159), 30, 30, 557.896);
+static const auto SS_FLV90 = ctrl::slalom::Shape(
+    ctrl::Position(63.6396, 63.6396, 1.5708),
+    ctrl::Position(41.9965, 41.9999, 1.5708), 21.6431, 21.6397, 390.623);
 
-static auto sd_FL90 =
-    SlalomDesigner(SlalomDesigner::Shape(Position(90, 90, M_PI / 2), 70));
-
-static auto sd_FL135 =
-    SlalomDesigner(SlalomDesigner::Shape(Position(45, 90, M_PI * 3 / 4), 80));
-
-static auto sd_FL180 =
-    SlalomDesigner(SlalomDesigner::Shape(Position(0, 90, M_PI), 90, 30));
-
-static auto sd_FLV90 = SlalomDesigner(SlalomDesigner::Shape(
-    Position(45 * std::sqrt(2), 45 * std::sqrt(2), M_PI / 2), 42));
+#define TO_STRING(VariableName) #VariableName
 
 int main(void) {
-  std::cout << sd_SL90 << std::endl;
-  std::cout << sd_FL45 << std::endl;
-  std::cout << sd_FL90 << std::endl;
-  std::cout << sd_FL135 << std::endl;
-  std::cout << sd_FL180 << std::endl;
-  std::cout << sd_FLV90 << std::endl;
+  SS_SL90.printDefinition(std::cout, TO_STRING(SS_SL90));
+  SS_FL45.printDefinition(std::cout, TO_STRING(SS_FL45));
+  SS_FL90.printDefinition(std::cout, TO_STRING(SS_FL90));
+  SS_FL135.printDefinition(std::cout, TO_STRING(SS_FL135));
+  SS_FL180.printDefinition(std::cout, TO_STRING(SS_FL180));
+  SS_FLV90.printDefinition(std::cout, TO_STRING(SS_FLV90));
 
-#define SLALOM_NUM 5
+#define SLALOM_NUM 1
 #if SLALOM_NUM == 0
-  auto &sd = sd_SL90;
+  auto sd = slalom::Trajectory(SS_SL90);
 #elif SLALOM_NUM == 1
-  auto &sd = sd_FL45;
+  auto sd = slalom::Trajectory(SS_FL45);
 #elif SLALOM_NUM == 2
-  auto &sd = sd_FL90;
+  auto sd = slalom::Trajectory(SS_FL90);
 #elif SLALOM_NUM == 3
-  auto &sd = sd_FL135;
+  auto sd = slalom::Trajectory(SS_FL135);
 #elif SLALOM_NUM == 4
-  auto &sd = sd_FL180;
+  auto sd = slalom::Trajectory(SS_FL180);
 #elif SLALOM_NUM == 5
-  auto &sd = sd_FLV90;
+  auto sd = slalom::Trajectory(SS_FLV90);
 #endif
   TrajectoryTracker tt;
   const float v = 600;
   sd.reset(v);
   tt.reset(v);
-  SlalomDesigner::State s;
+  slalom::State s;
   const float Ts = 0.001f;
   while (s.t < sd.t_end()) {
     sd.update(&s, Ts);
